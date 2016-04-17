c5a17c23d97d3ed762f57a3c789b236f0a1b293e
George sedky <georgesedky@Georges-MBP.fritz.box>
2016-04-17 20:11:35 +0200

    IMPLEMENT closeTable && MODIFY Block.setData to overwrite

--- src/minidb/recordmanager/RecordManager.java
+++ src/minidb/recordmanager/RecordManager.java
@@ -1,6 +1,8 @@
 package minidb.recordmanager;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.BitSet;
 
 import minidb.storagemanager.Block;
@@ -16,7 +18,8 @@ public class RecordManager implements IRecordManager{
 			dbf = (DBFile) sm.openFile(tableName);
 			Block metaBlock = (Block) sm.readFirstBlock(dbf);
 			String[] metaInfo = (new String(metaBlock.getData())).split("/");
-			MetaData mt = new MetaData(metaInfo.length-1);	
+			MetaData mt = new MetaData(metaInfo.length-1);
+			Character c = new Character((char) metaBlock.getData()[0]);
 			for(int i = 0; i < metaInfo.length-1; i++)
 			{
 				String[] col = metaInfo[i].split("$");
@@ -25,15 +28,53 @@ public class RecordManager implements IRecordManager{
 				mt.isKey[i] = Boolean.valueOf(col[2]);
 				mt.references[i] = col[3];
 			}
+			
 			mt.setSlotSize();
 			mt.setSlotsPerBlock(dbf.getBlockSize());
+			
+			byte[] tmpBytes = metaInfo[metaInfo.length-1].getBytes();
+			
+			int bytes = (int) Math.ceil(mt.slotsPerBlock/8.0);
+			
+			for(int j = 0; j < tmpBytes.length; j += bytes)
+				mt.blocks.set(j, BitSet.valueOf(Arrays.copyOfRange(tmpBytes, j, bytes)));
+			
 			mt.dbFile = dbf;
+			
+			if(mt.dbFile.getFileSize()/mt.dbFile.getBlockSize() == 1)
+			{
+				sm.appendEmptyBlock(mt.dbFile);
+				mt.addEmptyBlock();
+			}
+			
 			return mt;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}	
+	}
+	
+	private void closeTable(String tableName, MetaData mt)
+	{
+		StorageManager sm = new StorageManager();
+		try {
+			Block metaBlock = new Block();
+			
+			String metaInfo = "";
+			
+			for(int i = 0 ; i < mt.columnNames.length ; i++)
+				metaInfo += String.format("%s$%s$%s$%s/", mt.columnNames[i], mt.dataTypes[i], mt.isKey[i], mt.references[i]);
+			
+			for(BitSet b : mt.blocks)
+				metaInfo += new String(b.toByteArray());
+			
+			metaBlock.setData(metaInfo.getBytes());
+			sm.writeBlock(0, mt.dbFile, metaBlock);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -78,7 +119,6 @@ public class RecordManager implements IRecordManager{
 	@Override
 	public AbstractRecord[] getRecord(String tableName, String columnName, String dataType, String value) {
 		// TODO Auto-generated method stub
-		MetaData mt = openTable(tableName);
 		
 		return null;
 	}
@@ -115,7 +155,7 @@ class MetaData{
 	public boolean[] isKey;
 	public String[] references;
 	
-	public BitSet[] blocks;
+	public ArrayList<BitSet> blocks;
 	
 	public int slotSize;
 	public int slotsPerBlock;
@@ -160,5 +200,11 @@ class MetaData{
 	
 	public void setSlotsPerBlock(int blockSize){
 		slotsPerBlock = blockSize/slotSize;
+	}
+	
+	public void addEmptyBlock(){
+		BitSet b = new BitSet(slotsPerBlock);
+		b.set(0, b.size());
+		blocks.add(b);
 	}
 }
--- src/minidb/storagemanager/Block.java
+++ src/minidb/storagemanager/Block.java
@@ -18,14 +18,17 @@ public class Block extends AbstractBlock{
 	@Override
 	public void setData(byte[] d) {
 		// TODO Auto-generated method stub
-		int freeByte = -1;
-		
-		for(int i = 0; i < data.length && freeByte < 0; i++)
-			if(data[i] == 0)
-				freeByte = i;
-		
-		for(int i = freeByte, j = 0; j < d.length; i++, j++)
-			data[i] = d[j];
+//		int freeByte = -1;
+//		
+//		for(int i = 0; i < data.length && freeByte < 0; i++)
+//			if(data[i] == 0)
+//				freeByte = i;
+//		
+//		for(int i = freeByte, j = 0; j < d.length; i++, j++)
+//			data[i] = d[j];
+
+		for(int i = 0; i < d.length; i++)
+			data[i] = d[i];
 	}
 
 }
